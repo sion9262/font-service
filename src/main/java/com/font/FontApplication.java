11deeb4311085150c83c98b237c2b761befe0cc1
sion <parksion9262@gmail.com>
2022-06-06 00:40:03 +0900

    init

---
+++ src/main/java/com/font/FontApplication.java
@@ -0,0 +1,13 @@
+package com.font;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/font/FontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.font;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
